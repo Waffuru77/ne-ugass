7ea065d2055cc0b2822f71f03d2d65c641fdb8d9
Waffuru77 <waffuru77@gmail.com>
2021-06-06 15:42:17 +0400

    HI !!!! :D

---
+++ script/main.js
@@ -0,0 +1,47 @@
+function MenuFizetes() {
+    window.location.href = "./fizetes.html";
+}
+
+function MenuHome() {
+    window.location.href = "./index.html";
+}
+
+function MenuPremium() {
+    window.location.href = "./premium.html";
+}
+
+function MenuThankYou() {
+    window.location.href = "./thankyou.html";
+}
+
+
+
+function toggleOnOneYearWindow() {
+    let toggler = document.getElementById("oneYearWindow");
+    toggler.style.visibility = "visible";
+}
+
+function toggleOffOneYearWindow() {
+    let toggler = document.getElementById("oneYearWindow");
+    toggler.style.visibility = "hidden";
+}
+
+function toggleOnTenYearWindow() {
+    let toggler = document.getElementById("tenYearWindow");
+    toggler.style.visibility = "visible";
+}
+
+function toggleOffTenYearWindow() {
+    let toggler = document.getElementById("tenYearWindow");
+    toggler.style.visibility = "hidden";
+}
+
+function toggleOnTwentyYearWindow() {
+    let toggler = document.getElementById("twentyYearWindow");
+    toggler.style.visibility = "visible";
+}
+
+function toggleOffTwentyYearWindow() {
+    let toggler = document.getElementById("twentyYearWindow");
+    toggler.style.visibility = "hidden";
+}
