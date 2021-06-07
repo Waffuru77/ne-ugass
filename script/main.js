f8b133f097684abc98f3e7d3d97cb9b8e62cce92
Waffuru77 <waffuru77@gmail.com>
2021-06-07 21:16:20 +0400

    ok.
    did everything.

--- script/main.js
+++ script/main.js
@@ -1,51 +1,7 @@
-function MenuSubscription() {
-    window.location.href = "./subscription.html";
-}
-
 function MenuHome() {
     window.location.href = "./index.html";
 }
 
-function MenuPremium() {
-    window.location.href = "./premium.html";
-}
-
 function MenuGames() {
     window.location.href = "./games.html";
-}
-
-function MenuThankYou() {
-    window.location.href = "./thankyou.html";
-}
-
-
-
-function toggleOnOneYearWindow() {
-    let toggler = document.getElementById("oneYearWindow");
-    toggler.style.visibility = "visible";
-}
-
-function toggleOffOneYearWindow() {
-    let toggler = document.getElementById("oneYearWindow");
-    toggler.style.visibility = "hidden";
-}
-
-function toggleOnTenYearWindow() {
-    let toggler = document.getElementById("tenYearWindow");
-    toggler.style.visibility = "visible";
-}
-
-function toggleOffTenYearWindow() {
-    let toggler = document.getElementById("tenYearWindow");
-    toggler.style.visibility = "hidden";
-}
-
-function toggleOnTwentyYearWindow() {
-    let toggler = document.getElementById("twentyYearWindow");
-    toggler.style.visibility = "visible";
-}
-
-function toggleOffTwentyYearWindow() {
-    let toggler = document.getElementById("twentyYearWindow");
-    toggler.style.visibility = "hidden";
 }
