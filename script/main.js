0a16ef36369af126362fe5b2e1820b992c969b18
Waffuru77 <waffuru77@gmail.com>
2021-06-13 21:35:37 +0400

    bye

--- script/main.js
+++ script/main.js
@@ -1,3 +1,24 @@
+const colors = document.getElementsByClassName('colors');
+
+let i;
+for(i = 0;i<colors.length;i++) {
+    colors[i].addEventListener('click', changecolor)
+}
+
+function changecolor() {
+    let background1 = this.getAttribute('data-background1')
+    let background2 = this.getAttribute('data-background2')
+    let text= this.getAttribute('data-text')
+    let active= this.getAttribute('data-active')
+    let menubackground= this.getAttribute('data-menu-background')
+
+    document.documentElement.style.setProperty('--background1', background1)
+    document.documentElement.style.setProperty('--background2', background2)
+    document.documentElement.style.setProperty('--text', text)
+    document.documentElement.style.setProperty('--active', active)
+    document.documentElement.style.setProperty('--menu-background', menubackground)
+}
+
 function MenuHome() {
     window.location.href = "./index.html";
 }
